82eec54bfc6a5e817daf2b4a54ee8df36f3ec8a5
Nate Muth <37983038+analyticsnate@users.noreply.github.com>
2018-12-02 09:14:47 -0600

    small updates

--- pyDailyFormat.py
+++ pyDailyFormat.py
@@ -1,11 +1,10 @@
 import pandas as pd
 
-class dailyFile:
+class daily_file:
     """
-
     """
-    self.colsList = ['ID','YEAR','MONTH','ELEMENT']
-    self.widthsList = [11,4,2,4]
+    cols_list = ['ID','YEAR','MONTH','ELEMENT']
+    widths_list = [11,4,2,4]
 
     def __init__(self,f):
         """
@@ -13,23 +12,23 @@ class dailyFile:
         """
         self.dly = f
 
-        # add VALUE(n), MFLAG(n), QFLAG(n), and SFLAG(n) for each day of the month# add V 
+        # add VALUE(n), MFLAG(n), QFLAG(n), and SFLAG(n) for each day of the month# add V
         for n in range(1,32):
-            
+
             # append column names to colsList
-            self.colsList.append('VALUE{}'.format(n))
-            self.colsList.append('MFLAG{}'.format(n))
-            self.colsList.append('QFLAG{}'.format(n))
-            self.colsList.append('SFLAG{}'.format(n))
-            
+            self.cols_list.append('VALUE{}'.format(n))
+            self.cols_list.append('MFLAG{}'.format(n))
+            self.cols_list.append('QFLAG{}'.format(n))
+            self.cols_list.append('SFLAG{}'.format(n))
+
             # append column widths to widthsList
-            self.widthsList.append(5)
-            self.widthsList.append(1)
-            self.widthsList.append(1)
-            self.widthsList.append(1)
+            self.widths_list.append(5)
+            self.widths_list.append(1)
+            self.widths_list.append(1)
+            self.widths_list.append(1)
 
         # build dataframe
-        self.dfRaw = pd.read_fwf(
+        self.df_raw = pd.read_fwf(
                             f
                             ,header=None
                             ,widths=self.widthsList
@@ -39,7 +38,7 @@ class dailyFile:
         self.df = pd.DataFrame(columns=['ID','YEAR','MONTH','ELEMENT','VALUE','DAY'])
 
         for i in range(1,32):
-            df_Append = self.dfRaw.filter(['ID','YEAR','MONTH','ELEMENT','VALUE{}'.format(i)])
-            df_Append.rename(columns={'VALUE{}'.format(i):'VALUE'},inplace=True)
-            df_Append['DAY'] = i
-            self.df = self.df.append(df_Append)
+            df_append = self.df_raw.filter(['ID','YEAR','MONTH','ELEMENT','VALUE{}'.format(i)])
+            df_append.rename(columns={'VALUE{}'.format(i):'VALUE'},inplace=True)
+            df_append['DAY'] = i
+            self.df = self.df.append(df_append)
